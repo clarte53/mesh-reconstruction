3621271ba02451f4de9dd5430f38b5848abffb12
Lionel <lionel_dominjon@yahoo.com>
2019-10-16 10:25:19 +0200

    Added simple realtime tessellation of the point cloud

---
+++ Scripts/MeshedPointCloudRenderer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class MeshedPointCloudRenderer : PointCloudRenderer
+{
+	[Range(0.001f, 0.3f)]
+	[SerializeField]
+	float maxEdgeLength = 0.05f;
+    
+    override protected void Update()
+    {
+		CLARTE.Dev.Profiling.Profiler.Start("MeshedPointCloudRenderer");
+		vertexBufferGenerator.SetFloat("maxEdgeLength", maxEdgeLength);
+
+		base.Update();
+		CLARTE.Dev.Profiling.Profiler.Stop("MeshedPointCloudRenderer");
+	}
+
+	override protected int ComputeVertexBufferMaxSize()
+	{
+		return (width - 1) * (height - 1) * 2 * 3;
+	}
+}
