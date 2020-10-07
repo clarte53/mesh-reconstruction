8a31b6be6667fb5fdc953048f63fdb9055e77520
karine <karine@clarte.asso.fr>
2020-10-07 13:58:01 +0200

    Scripts to handle kinect for azure input

---
+++ Scripts/Kinect4A_PointCloudProvider.cs
@@ -0,0 +1,76 @@
+﻿using com.rfilkov.kinect;
+using System.Collections;
+using System.Collections.Generic;
+using Microsoft.Azure.Kinect.Sensor;
+using UnityEngine;
+
+public class Kinect4A_PointCloudProvider : PointCloudProvider
+{
+
+    public KinectManager kinectManager;
+    public int sensorIndex;
+
+    private bool texInitialized = false;
+    private Texture2D tex;
+    private ushort[] rawData;
+    private Color[] pixels;
+    private int imWidth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rawData = kinectManager.GetRawDepthMap(sensorIndex);
+        colorTexture = kinectManager.GetColorImageTex(sensorIndex);
+
+        if (! texInitialized)
+        {
+            tex = new Texture2D(kinectManager.GetDepthImageWidth(sensorIndex), kinectManager.GetDepthImageHeight(sensorIndex));
+            pixels = new Color[rawData.Length];
+            imWidth = kinectManager.GetDepthImageWidth(sensorIndex);
+            texInitialized = true;
+        }
+        
+
+        for (int i = 0; i < rawData.Length; i++)
+        {
+            int x = Mathf.FloorToInt(i / imWidth);
+            int y = i % imWidth;
+            Vector3 pos = kinectManager.MapDepthPointToSpaceCoords(sensorIndex, new Vector2(x, y), rawData[i], false);
+            //Vector2 uv = kinectManager.MapDepthPointToColorCoords(sensorIndex, new Vector2(i, j), rawValue);
+            pixels[i] = new Color(pos.x, pos.y, pos.z);//, EncodeUV(uv));
+        }
+        // = new Texture2D(depthTexture.width, depthTexture.height, TextureFormat.ARGB32, true);
+        //for (int i = 0; i < depthTexture.width; i++)
+        //{
+        //    for (int j = 0; j < depthTexture.height; j++)
+        //    {
+        //        ushort rawValue = rawData[j * depthTexture.width + i];
+        //        Vector3 pos = kinectManager.MapDepthPointToSpaceCoords(sensorIndex, new Vector2(i, j), rawValue, false);
+        //        //Vector2 uv = kinectManager.MapDepthPointToColorCoords(sensorIndex, new Vector2(i, j), rawValue);
+        //        Color c = new Color(pos.x, pos.y, pos.z);//, EncodeUV(uv));
+        //        tex.SetPixel(i, j, c);
+
+        //    }
+        //}
+        
+        tex.SetPixels( pixels);
+        tex.Apply();
+        vertexTexture = tex;
+    }
+
+    float EncodeUV(Vector2 uv)
+    {
+        //quantize from 0-1 floats, to 0-65535 integers, which can be represented in 16 bits
+        uint u = (uint)Mathf.Round(uv.x * 65535.0f);
+        uint v = (uint)Mathf.Round(uv.y * 65535.0f);
+
+        uint encoded = u << 16 | v;
+
+        return (float)encoded;
+    }
+}
---
+++ Scripts/Kinect4A_v2.cs
@@ -0,0 +1,168 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Microsoft.Azure.Kinect.Sensor;
+using System.Threading.Tasks;
+
+public class Kinect4A_v2 : PointCloudProvider
+{
+    public ComputeShader TextureGenerator;
+
+    private ComputeBuffer inputPositionBuffer;
+    //Variable for handling Kinect
+    Device kinect;
+    //Number of all points of PointCloud 
+    int depthWidth;
+    int depthHeight;
+    int colorWidth;
+    int colorHeight;
+    //Used to draw a set of points
+    Mesh mesh;
+    //Array of coordinates for each point in PointCloud
+    Vector3[] vertices;
+    //Array of colors corresponding to each point in PointCloud
+    Color[] colors;
+    //List of indexes of points to be rendered
+    int[] indices;
+    //Class for coordinate transformation(e.g.Color-to-depth, depth-to-xyz, etc.)
+    Transformation transformation;
+    protected RenderTexture vertexRenderTexture;
+    protected int vertexTextureGeneratorKernelId;
+
+    void Start()
+    {
+        //The method to initialize Kinect
+        InitKinect();
+        //Initialization for point cloud rendering
+        InitValues();
+    }
+
+    private void Update()
+    {
+        Capture capture = kinect.GetCapture();
+        
+        Texture2D colorTex = new Texture2D(colorWidth, colorHeight);
+        Texture2D depthTex = new Texture2D(depthWidth, depthHeight);
+
+        //Getting color information
+        Image colorImage = capture.Color;
+        BGRA[] colorArray = colorImage.GetPixels<BGRA>().ToArray();
+        ////Getting vertices of point cloud
+        Image xyzImage = transformation.DepthImageToPointCloud(capture.Depth);
+        Short3[] xyzArray = xyzImage.GetPixels<Short3>().ToArray();
+
+        //Image depthToColorImage = transformation.DepthImageToColorCamera(capture.Depth);
+
+        for (int i = 0; i < depthWidth * depthHeight; i++)
+        {
+            vertices[i].x = xyzArray[i].X * 0.001f;
+            vertices[i].y = -xyzArray[i].Y * 0.001f;
+            vertices[i].z = xyzArray[i].Z * 0.001f;
+
+            int x = Mathf.FloorToInt(i / depthWidth);
+            int y = i % depthWidth;
+            //vertices[i].a = EncodeUV(new Vector2(Mathf.Floor((x / depthWidth) * colorWidth),
+            //    Mathf.Floor( (y / depthHeight) * colorHeight)));
+           // vertices[i].a = EncodeUV(new Vector2(x, y));
+        }
+        
+        for (int i = 0; i < colorWidth * colorHeight; i++)
+        {
+            
+            colors[i].b = colorArray[i].B / 255f;
+            colors[i].g = colorArray[i].G / 255f;
+            colors[i].r = colorArray[i].R / 255f;
+        }
+        colorTex.SetPixels(colors);
+        colorTex.Apply();
+        colorTexture = colorTex;
+
+        ComputePointCloudData();
+    }
+
+    //Initialization of Kinect
+    private void InitKinect()
+    {
+        //Connect with the 0th Kinect
+        kinect = Device.Open(0);
+
+        //Setting the Kinect operation mode and starting it
+        kinect.StartCameras(new DeviceConfiguration
+        {
+            ColorFormat = ImageFormat.ColorBGRA32,
+            ColorResolution = ColorResolution.R720p,
+            DepthMode = DepthMode.NFOV_Unbinned,
+            SynchronizedImagesOnly = true,
+            CameraFPS = FPS.FPS30
+        });
+        //Access to coordinate transformation information
+        transformation = kinect.GetCalibration().CreateTransformation();
+    }
+
+    //Prepare to draw point cloud.
+    private void InitValues()
+    {
+        //Get the width and height of the Depth image and calculate the number of all points
+        depthWidth = kinect.GetCalibration().DepthCameraCalibration.ResolutionWidth;
+        depthHeight = kinect.GetCalibration().DepthCameraCalibration.ResolutionHeight;
+        int num = depthWidth * depthHeight;
+
+        colorWidth = kinect.GetCalibration().ColorCameraCalibration.ResolutionWidth;
+        colorHeight = kinect.GetCalibration().ColorCameraCalibration.ResolutionHeight;
+        
+        //Allocation of vertex and color storage space for the total number of pixels in the depth image
+        vertices = new Vector3[depthWidth * depthHeight];
+        colors = new Color[colorWidth * colorHeight];
+    }
+    
+    float EncodeUV(Vector2 uv)
+    {
+        //quantize from 0-1 floats, to 0-65535 integers, which can be represented in 16 bits
+        uint u = (uint)Mathf.Round(uv.x * 65535.0f);
+        uint v = (uint)Mathf.Round(uv.y * 65535.0f);
+
+        uint encoded = u << 16 | v;
+
+        return (float)encoded;
+    }
+
+    //Stop Kinect as soon as this object disappear
+    private void OnDestroy()
+    {
+        kinect.StopCameras();
+    }
+
+    private void ComputePointCloudData()
+    {
+        // Initialize compute buffers if needed
+        if (inputPositionBuffer == null)
+        {
+            inputPositionBuffer = new ComputeBuffer(depthWidth * depthHeight, 3 * sizeof(float));
+        }
+
+        // Initialize rendertexture if needed
+        if (vertexRenderTexture == null)
+        {
+            vertexRenderTexture = new RenderTexture(depthWidth, depthHeight, 0, RenderTextureFormat.ARGBFloat);
+            vertexRenderTexture.enableRandomWrite = true;
+            vertexRenderTexture.useMipMap = false;
+            vertexRenderTexture.filterMode = FilterMode.Point;
+            vertexRenderTexture.Create();
+
+            vertexTexture = (Texture)vertexRenderTexture;
+        }
+
+        // Vertex generator
+        vertexTextureGeneratorKernelId = TextureGenerator.FindKernel("VertexGenerator");
+        TextureGenerator.SetBuffer(vertexTextureGeneratorKernelId, "Input_positionData", inputPositionBuffer);
+        inputPositionBuffer.SetData(vertices);
+
+        TextureGenerator.SetInt("depthWidth", depthWidth);
+        TextureGenerator.SetInt("depthHeight", depthHeight);
+        TextureGenerator.SetInt("colorWidth", colorWidth);
+        TextureGenerator.SetInt("colorHeight", colorHeight);
+
+        TextureGenerator.SetTexture(vertexTextureGeneratorKernelId, "Output_vertexTexture", vertexRenderTexture);
+        TextureGenerator.Dispatch(vertexTextureGeneratorKernelId, depthWidth, depthHeight, 1);
+    }
+}
