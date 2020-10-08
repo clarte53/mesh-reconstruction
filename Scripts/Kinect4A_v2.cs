ede87c46d76db5e8714993ab02c4f7fa6279d656
karine <karine@clarte.asso.fr>
2020-10-08 17:51:40 +0200

    Sneding position buffer to the shader

--- Scripts/Kinect4A_v2.cs
+++ Scripts/Kinect4A_v2.cs
@@ -1,16 +1,16 @@
-﻿using System.Collections;
+﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
 using Microsoft.Azure.Kinect.Sensor;
 using CLARTE.Dev.Profiling;
 using System.Threading.Tasks;
+using Windows.Kinect;
 
 public class Kinect4A_v2 : PointCloudProvider
 {
     public ComputeShader TextureGenerator;
 
     private ComputeBuffer inputPositionBuffer;
-    private ComputeBuffer inputUVBuffer;
     private ComputeBuffer inputColorBuffer;
     //Variable for handling Kinect
 
@@ -22,11 +22,13 @@ public class Kinect4A_v2 : PointCloudProvider
     //Used to draw a set of points
     Mesh mesh;
     //Array of coordinates for each point in PointCloud
-    float[] uvCoordinates;
     Vector3[] vertices;
+    Color[] testArray;
+    byte[] vs;
     //Array of colors corresponding to each point in PointCloud
-    Color[] colors;
-
+    BGRA[] colors;
+    public Texture2D tex;
+    public Renderer quad;
 
     //Class for coordinate transformation(e.g.Color-to-depth, depth-to-xyz, etc.)
     Transformation transformation;
@@ -50,31 +52,33 @@ public class Kinect4A_v2 : PointCloudProvider
 
         //Getting color information
         Image colorImage = transformation.ColorImageToDepthCamera(capture);
-        BGRA[] colorArray = colorImage.GetPixels<BGRA>().ToArray();
+        colors = colorImage.GetPixels<BGRA>().ToArray();
 
         //Getting vertices of point cloud
         Image xyzImage = transformation.DepthImageToPointCloud(capture.Depth);
         Short3[] xyzArray = xyzImage.GetPixels<Short3>().ToArray();
-        
-        //Image depthToColorImage = transformation.DepthImageToColorCamera(capture.Depth);
-        Profiler.Start("vertex loop");
+        xyzImage.CopyBytesTo(vs, 0, 0, vs.Length);
+
         for (int i = 0; i < depthWidth * depthHeight; i++)
         {
             vertices[i].x = xyzArray[i].X * 0.001f;
             vertices[i].y = -xyzArray[i].Y * 0.001f;
             vertices[i].z = xyzArray[i].Z * 0.001f;
 
-            colors[i].b = colorArray[i].B / 255f;
-            colors[i].g = colorArray[i].G / 255f;
-            colors[i].r = colorArray[i].R / 255f;
+            //int r = BitConverter.ToInt16(new byte[2] { vs[6 * i], vs[6 * i + 1] }, 0);
+            //int g = BitConverter.ToInt16(new byte[2] { vs[6 * i + 2], vs[6 * i + 3] }, 0);
+            //int b = BitConverter.ToInt16(new byte[2] { vs[6 * i + 4], vs[6 * i + 5] }, 0);
 
-            int x = Mathf.FloorToInt(i / depthWidth);
-            int y = i % depthWidth;
-            uvCoordinates[i] = EncodeUV(new Vector2(x, y));
+            //testArray[i] = new Color(r * 0.001f, g * 0.001f, b * 0.001f);
         }
 
-        Profiler.Stop("vertex loop");
         ComputePointCloudData();
+        quad.material.mainTexture = vertexTexture;
+    }
+
+    public static Vector3 ShortToVector(Short3 sh)
+    {
+        return new Vector3(sh.X, -sh.Y, sh.Z) * 0.001f;
     }
 
     private void OnDisable()
@@ -105,6 +109,7 @@ public class Kinect4A_v2 : PointCloudProvider
         });
         //Access to coordinate transformation information
         transformation = kinect.GetCalibration().CreateTransformation();
+        
     }
 
     //Prepare to draw point cloud.
@@ -117,8 +122,9 @@ public class Kinect4A_v2 : PointCloudProvider
 
         //Allocation of vertex and color storage space for the total number of pixels in the depth image
         vertices = new Vector3[depthWidth * depthHeight];
-        colors = new Color[depthWidth * depthHeight];
-        uvCoordinates = new float[depthWidth * depthHeight];
+        vs = new byte[depthWidth * depthHeight * 3 * 2]; // short3 = 3 * 2byte
+        testArray = new Color[depthWidth * depthHeight];
+        tex = new Texture2D(depthWidth, depthHeight);
     }
     
     private void ComputePointCloudData()
@@ -126,17 +132,13 @@ public class Kinect4A_v2 : PointCloudProvider
         // Initialize compute buffers if needed
         if (inputPositionBuffer == null)
         {
-            inputPositionBuffer = new ComputeBuffer(depthWidth * depthHeight, 3 * sizeof(float)); // Vector3
-        }
-
-        if (inputUVBuffer == null)
-        {
-            inputUVBuffer = new ComputeBuffer(depthWidth * depthHeight, sizeof(float)); // float
+            inputPositionBuffer = new ComputeBuffer(depthWidth * depthHeight * 3 / 2, sizeof(uint)); // bytes
+            //inputPositionBuffer = new ComputeBuffer(depthWidth * depthHeight, 3 * sizeof(float)); // vectors
         }
 
         if (inputColorBuffer == null)
         {
-            inputColorBuffer = new ComputeBuffer(depthWidth * depthHeight, 4 * sizeof(float)); // Color
+            inputColorBuffer = new ComputeBuffer(depthWidth * depthHeight, sizeof(uint)); // Color
         }
 
         // Initialize rendertexture if needed
@@ -165,11 +167,9 @@ public class Kinect4A_v2 : PointCloudProvider
         // Vertex generator
         vertexTextureGeneratorKernelId = TextureGenerator.FindKernel("VertexGenerator");
         TextureGenerator.SetBuffer(vertexTextureGeneratorKernelId, "Input_positionData", inputPositionBuffer);
-        inputPositionBuffer.SetData(vertices);
+        inputPositionBuffer.SetData(vs);
+        //inputPositionBuffer.SetData(vertices);
 
-        TextureGenerator.SetBuffer(colorTextureGeneratorKernelId, "Input_uvData", inputUVBuffer);
-        inputUVBuffer.SetData(uvCoordinates);
-        
         TextureGenerator.SetInt("depthWidth", depthWidth);
         TextureGenerator.SetInt("depthHeight", depthHeight);
         TextureGenerator.SetInt("colorWidth", depthWidth);
