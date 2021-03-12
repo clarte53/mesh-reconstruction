9fa5665c001f74fea3540585b456b5ebf9f86e57
Lionel <lionel_dominjon@yahoo.com>
2021-03-13 00:30:06 +0100

    Exposed mesh rendering angle thresholds in the inspector

--- Scripts/MeshedPointCloudRenderer.cs
+++ Scripts/MeshedPointCloudRenderer.cs
@@ -5,13 +5,27 @@ public class MeshedPointCloudRenderer : PointCloudRenderer
 	[Range(0.001f, 0.3f)]
 	[SerializeField]
 	float maxEdgeLength = 0.05f;
+
+	[Range(0.0f, 90.0f)]
+	[SerializeField]
+	float startFadeAngle = 63.0f;
+
+	[Range(0.0f, 90.0f)]
+	[SerializeField]
+	float endFadeAngle = 80.0f;
     
     override protected void Update()
     {
 		CLARTE.Dev.Profiling.Profiler.Start("MeshedPointCloudRenderer");
+
 		vertexBufferGenerator.SetFloat("maxEdgeLength", maxEdgeLength);
 
+		vertexBufferGenerator.SetFloat("startFadeAngle", startFadeAngle * Mathf.Deg2Rad);
+
+		vertexBufferGenerator.SetFloat("endFadeAngle", endFadeAngle * Mathf.Deg2Rad);
+
 		base.Update();
+
 		CLARTE.Dev.Profiling.Profiler.Stop("MeshedPointCloudRenderer");
 	}
 
