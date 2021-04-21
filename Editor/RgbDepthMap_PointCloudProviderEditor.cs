4ff903563e63f075e14ba1739ff798d8eb476905
Karine <perrard@clarte-lab.fr>
2021-04-21 15:50:54 +0200

    Add texture support to RgbDepthMap_PointCloudProvider; Add edge filtering option

---
+++ Editor/RgbDepthMap_PointCloudProviderEditor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(RgbDepthMap_PointCloudProvider))]
+public class RgbDepthMap_PointCloudProviderEditor : UnityEditor.Editor
+{
+
+    public override void OnInspectorGUI()
+	{
+		DrawDefaultInspector();
+		RgbDepthMap_PointCloudProvider myScript = (RgbDepthMap_PointCloudProvider)target;
+
+		if(myScript.processDepthEdge)
+		{
+			myScript.edgeSmoothingShader = EditorGUILayout.ObjectField("Edge Filtering Shader", myScript.edgeSmoothingShader,
+				typeof(ComputeShader), true) as ComputeShader;
+			myScript.laplacianThreshold = EditorGUILayout.FloatField("Laplacian threshold", myScript.laplacianThreshold);
+			myScript.dilateRadius = EditorGUILayout.IntField("Dilate radius", myScript.dilateRadius);
+		}
+	}
+}
--- Scripts/RgbDepthMap_PointCloudProvider.cs
+++ Scripts/RgbDepthMap_PointCloudProvider.cs
@@ -5,7 +5,7 @@ public class RgbDepthMap_PointCloudProvider : PointCloudProvider
 
 	public float slide_u = 0.0125f;
 	public float slide_v = -0.005f;
-	private Texture2D depthTexture;
+	private Texture depthTexture;
 
 	public ComputeShader TextureGenerator;
 
@@ -17,16 +17,35 @@ public class RgbDepthMap_PointCloudProvider : PointCloudProvider
 
 	protected int vertexTextureGeneratorKernelId;
 
+	public bool processDepthEdge = false;
+	public ComputeShader edgeSmoothingShader { get; set; }
+	public float laplacianThreshold { get; set; } = 100;
+	public int dilateRadius { get; set; } = 2;
+	private Texture processedEdgeTexture;
+
 	public void Awake()
 	{
 		TextureGenerator = ComputeShader.Instantiate(TextureGenerator);
 	}
 
-	public void SetData(PointCloudRgbDepthData pcData)
+	private void SetData(PointCloudRgbDepthData pcData)
 	{
 		SetDepthParameters(pcData.depthSize[0], pcData.depthSize[1], pcData.depthIntrinsec[0], pcData.depthIntrinsec[1], pcData.depthIntrinsec[2], pcData.depthIntrinsec[3]);
 		SetImageParameters(pcData.imageSize[0], pcData.imageSize[1], pcData.imageIntrinsec[0], pcData.imageIntrinsec[1], pcData.imageIntrinsec[2], pcData.imageIntrinsec[3]);
 		SetImagePoseInDepthSpace(pcData.imagePoseToDepthCoords);
+	}
+
+	public void SetData(PointCloudRgbDepthData_GPU pcData)
+	{
+		SetData((PointCloudRgbDepthData)pcData);
+
+		CreatePointCloud(pcData.depthData, pcData.colorData);
+	}
+
+	public void SetData(PointCloudRgbDepthData_CPU pcData)
+	{
+		SetData((PointCloudRgbDepthData)pcData);
+
 		CreatePointCloud(pcData.depthData, pcData.colorData);
 	}
 
@@ -59,7 +78,7 @@ public class RgbDepthMap_PointCloudProvider : PointCloudProvider
 		imagePoseToDepthCoords.SetRow(3, new Vector4(m[12], m[13], m[14], m[15]));
 	}
 
-	private void CreatePointCloud(float[] depthData, byte[] imageData)
+	void InitTextures()
 	{
 		if (vertexTexture == null)
 		{
@@ -80,18 +99,54 @@ public class RgbDepthMap_PointCloudProvider : PointCloudProvider
 			TextureGenerator.SetInt("ColorWidth", colorTexture.width);
 			TextureGenerator.SetInt("ColorHeight", colorTexture.height);
 		}
+	}
 
-		var depth_data = depthTexture.GetRawTextureData<float>();
+	private Texture ProcessEdge(Texture depth_texture)
+	{
+		if(processedEdgeTexture == null)
+		{
+			processedEdgeTexture = new RenderTexture(depthSize.x, depthSize.y, 0, RenderTextureFormat.RHalf);
+			((RenderTexture)processedEdgeTexture).enableRandomWrite = true;
+			((RenderTexture)processedEdgeTexture).useMipMap = false;
+			((RenderTexture)processedEdgeTexture).Create();
+		}
+
+		edgeSmoothingShader.SetFloat("threshold", laplacianThreshold);
+		edgeSmoothingShader.SetInt("radius", dilateRadius);
+		int kernel_id = edgeSmoothingShader.FindKernel("DepthProcessing");
+		edgeSmoothingShader.SetTexture(kernel_id, "InputDepth", depth_texture);
+		edgeSmoothingShader.SetTexture(kernel_id, "OutputTexture", processedEdgeTexture);
+		edgeSmoothingShader.Dispatch(kernel_id, depthTexture.width, depthTexture.height, 1);
+
+		return processedEdgeTexture;
+	}
+	private void CreatePointCloud(Texture depthData, Texture imageData)
+	{
+		InitTextures();
+		Texture depth_texture = depthData;
+		if(processDepthEdge)
+		{
+			depth_texture = ProcessEdge(depthData);
+		}
+		colorTexture = imageData;
+		vertexTextureGeneratorKernelId = TextureGenerator.FindKernel("VertexGenerator");
+		TextureGenerator.SetFloats("SlideUV", slide_u, slide_v);
+		TextureGenerator.SetTexture(vertexTextureGeneratorKernelId, "Input_Depth", depth_texture);
+		TextureGenerator.SetTexture(vertexTextureGeneratorKernelId, "Output_vertexTexture", (RenderTexture)vertexTexture);
+		TextureGenerator.Dispatch(vertexTextureGeneratorKernelId, depthTexture.width / 8, depthTexture.height / 8, 1);
+	}
+
+	private void CreatePointCloud(float[] depthData, byte[] imageData)
+	{
+		InitTextures();
+
+		var depth_data = ((Texture2D)depthTexture).GetRawTextureData<float>();
 		depth_data.CopyFrom(depthData);
-		depthTexture.Apply();
+		((Texture2D)depthTexture).Apply();
 
 		((Texture2D)colorTexture).LoadRawTextureData(imageData);
 		((Texture2D)colorTexture).Apply();
 
-		vertexTextureGeneratorKernelId = TextureGenerator.FindKernel("VertexGenerator");
-		TextureGenerator.SetFloats("SlideUV", slide_u, slide_v);
-		TextureGenerator.SetTexture(vertexTextureGeneratorKernelId, "Input_Depth", depthTexture);
-		TextureGenerator.SetTexture(vertexTextureGeneratorKernelId, "Output_vertexTexture", (RenderTexture)vertexTexture);
-		TextureGenerator.Dispatch(vertexTextureGeneratorKernelId, depthTexture.width / 8, depthTexture.height / 8, 1);
+		CreatePointCloud(depthTexture, colorTexture);
 	}
 }
