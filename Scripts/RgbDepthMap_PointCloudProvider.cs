8636dffc516ed94f566ccd4606cae0abe143326c
Karine <perrard@clarte-lab.fr>
2021-04-22 12:30:26 +0200

    code documentation

--- Scripts/RgbDepthMap_PointCloudProvider.cs
+++ Scripts/RgbDepthMap_PointCloudProvider.cs
@@ -16,12 +16,13 @@ public class RgbDepthMap_PointCloudProvider : PointCloudProvider
 	private Matrix4x4 imagePoseToDepthCoords;
 
 	protected int vertexTextureGeneratorKernelId;
-	
+
+	// Variables for edge smoothing
 	public bool processDepthEdge = false;
 	[HideInInspector]
 	public ComputeShader edgeSmoothingShader;
 	[HideInInspector]
-	public float laplacianThreshold  = 100;
+	public float laplacianThreshold = 100;
 	[HideInInspector]
 	public int dilateRadius = 2;
 	private Texture processedEdgeTexture;
@@ -29,8 +30,16 @@ public class RgbDepthMap_PointCloudProvider : PointCloudProvider
 	public void Awake()
 	{
 		TextureGenerator = ComputeShader.Instantiate(TextureGenerator);
+		if (processDepthEdge)
+		{
+			edgeSmoothingShader = ComputeShader.Instantiate(edgeSmoothingShader);
+		}
 	}
 
+	/// <summary>
+	/// Set values related to depth and color images: image sizes, cameras intrinsics and transformation matrix between images.
+	/// </summary>
+	/// <param name="pcData"></param>
 	private void SetData(PointCloudRgbDepthData pcData)
 	{
 		SetDepthParameters(pcData.depthSize[0], pcData.depthSize[1], pcData.depthIntrinsec[0], pcData.depthIntrinsec[1], pcData.depthIntrinsec[2], pcData.depthIntrinsec[3]);
@@ -38,6 +47,10 @@ public class RgbDepthMap_PointCloudProvider : PointCloudProvider
 		SetImagePoseInDepthSpace(pcData.imagePoseToDepthCoords);
 	}
 
+	/// <summary>
+	/// Update point cloud from depth and color images that are stored on CPU. Data is passed as native arrays
+	/// </summary>
+	/// <param name="pcData"></param>
 	public void SetData(PointCloudRgbDepthData_GPU pcData)
 	{
 		SetData((PointCloudRgbDepthData)pcData);
@@ -45,6 +58,10 @@ public class RgbDepthMap_PointCloudProvider : PointCloudProvider
 		CreatePointCloud(pcData.depthData, pcData.colorData);
 	}
 
+	/// <summary>
+	/// Update point cloud from depth and color images that are stored on GPU. Data is passed as Textures
+	/// </summary>
+	/// <param name="pcData"></param>
 	public void SetData(PointCloudRgbDepthData_CPU pcData)
 	{
 		SetData((PointCloudRgbDepthData)pcData);
@@ -104,9 +121,15 @@ public class RgbDepthMap_PointCloudProvider : PointCloudProvider
 		}
 	}
 
+	/// <summary>
+	/// Create a mask on the edges of the texture depth map using a Laplacian operator followed by a Dilate operator. 
+	/// The depth of the pixels that are affected by the mask is set to 0;
+	/// </summary>
+	/// <param name="depth_texture"></param>
+	/// <returns></returns>
 	private Texture ProcessEdge(Texture depth_texture)
 	{
-		if(processedEdgeTexture == null)
+		if (processedEdgeTexture == null)
 		{
 			processedEdgeTexture = new RenderTexture(depthSize.x, depthSize.y, 0, RenderTextureFormat.RHalf);
 			((RenderTexture)processedEdgeTexture).enableRandomWrite = true;
@@ -123,11 +146,18 @@ public class RgbDepthMap_PointCloudProvider : PointCloudProvider
 
 		return processedEdgeTexture;
 	}
+
+	/// <summary>
+	/// Process data that is stored on GPU to create a point cloud out of the depth map. 
+	/// Point UV volor mapping is computed using cameras intrinsics and transformation.
+	/// </summary>
+	/// <param name="depthData"></param>
+	/// <param name="imageData"></param>
 	private void CreatePointCloud(Texture depthData, Texture imageData)
 	{
 		InitTextures();
 		Texture depth_texture = depthData;
-		if(processDepthEdge)
+		if (processDepthEdge)
 		{
 			depth_texture = ProcessEdge(depthData);
 		}
@@ -139,6 +169,11 @@ public class RgbDepthMap_PointCloudProvider : PointCloudProvider
 		TextureGenerator.Dispatch(vertexTextureGeneratorKernelId, depthTexture.width / 8, depthTexture.height / 8, 1);
 	}
 
+	/// <summary>
+	/// Transfer the CPU data to GPU so that it can be processed by compute shader.
+	/// </summary>
+	/// <param name="depthData"></param>
+	/// <param name="imageData"></param>
 	private void CreatePointCloud(float[] depthData, byte[] imageData)
 	{
 		InitTextures();
